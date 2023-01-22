2dbe7b16a305738fd76b0bbc50baaf1e0e5cb51c
vishnuvps <vishnuprashob.s@gmail.com>
2023-01-22 16:52:06 +0530

    new code edited in dashboard component test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { DashboardComponent } from '../../src/app/dashboard/dashboard.component';
+const routes: Routes = [
+{ path: 'dashboard', component: DashboardComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
